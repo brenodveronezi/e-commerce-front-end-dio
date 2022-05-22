10964d44e69677b184f3b7cd94f2bdb5c1972e94
Breno <brenodveronezi@gmail.com>
2022-05-22 15:48:02 -0300

    first commit

---
+++ src/app/components/bookstore-app/product-list/product-item/product-item.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { BooksService } from '../product-list.component.service';
+import { Book } from './model/book';
+
+@Component({
+  selector: 'app-product-item',
+  templateUrl: './product-item.component.html',
+  styleUrls: ['./product-item.component.css']
+})
+export class ProductItemComponent implements OnInit {
+
+
+  @Input()
+  livro!: Book;
+  constructor() { }
+
+  ngOnInit(): void {
+
+    }
+
+}
